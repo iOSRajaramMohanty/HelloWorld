be3dd5580bc4b3a91ab87e41a5d545f1d2c07853
iOSRajaramMohanty <HanRiver@Avinandans-MacBook-Pro-2.local>
2015-07-10 17:44:08 +0530

    test Up

---
+++ Demo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Avinandan Roy on 7/1/15.
+//  Copyright (c) 2015 Avinandan Roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var butonAct: UIButton!
+
+    @IBAction func butt(sender: AnyObject) {
+        
+    }
+    @IBOutlet weak var support: UIButton!
+    @IBAction func supportAction(sender: AnyObject) {
+    }
+}
+
